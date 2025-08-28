aef573a2ed3cc116c8609c274ae087d52a6450fe
pejman <pejman.kiamehr@gmail.com>
2025-08-28 13:53:57 +0330

    chenge link project

--- components/sections/Work.jsx
+++ components/sections/Work.jsx
@@ -9,7 +9,7 @@ const Work = ({ dict }) => {
   const { isDarkMode } = useTheme();
 
   return (
-    <motion.div initial={{ opacity:0 }} whileInView={{ opacity:1 }} transition={{ duration: 1 }} id="work" className=" w-full px-[12%] py-10 scroll-mt-18">
+    <motion.div initial={{ opacity: 0 }} whileInView={{ opacity: 1 }} transition={{ duration: 1 }} id="work" className=" w-full px-[12%] py-10 scroll-mt-18">
       <motion.h4 initial={{ y: -20, opacity: 0 }} whileInView={{ y: 0, opacity: 1 }} transition={{ duration: 0.5, delay: 0.1 }} className=" text-center mb-2 text-lg font-Ovo">
         {dict.work.h4}
       </motion.h4>
@@ -25,12 +25,11 @@ const Work = ({ dict }) => {
         transition={{ duration: 0.6, delay: 0.7 }}
         className="grid [grid-template-columns:repeat(auto-fit,minmax(300px,1fr))] my-6 gap-5 dark:text-black"
       >
-        <motion.div
-          whileFocus={{ scale: 1.05 }}
-          transition={{ duration: 0.3 }}
-          className='aspect-square  bg-[url("/images/project-1.png")] bg-no-repeat bg-center  bg-cover rounded-lg relative cursor-pointer group'
-        >
-          <div className=" bg-white w-10/12 rounded-md absolute bottom-5 left-1/2 -translate-x-1/2 py-3 px-5 flex items-center justify-between duration-500 group-hover:bottom-7">
+        <motion.div whileFocus={{ scale: 1.05 }} transition={{ duration: 0.3 }} className='aspect-square  bg-[url("/images/project-1.png")] bg-no-repeat bg-center  bg-cover rounded-lg relative group'>
+          <a
+            href="https://project1.kiamehr.dev/"
+            className=" bg-white w-10/12 rounded-md absolute bottom-5 left-1/2 -translate-x-1/2 py-3 px-5 flex items-center justify-between duration-500 group-hover:bottom-7"
+          >
             <div>
               <h2 className=" font-semibold">{dict.work.projecttitle1}</h2>
               <p className=" text-xs text-gray-700">{dict.work.projectdescription1}</p>
@@ -38,29 +37,21 @@ const Work = ({ dict }) => {
             <div className=" border rounded-full border-black w-11 aspect-square flex items-center justify-center shadow:[2px_2px_0_#000] group-hover:bg-lime-300 transition">
               <Image src={assets.send_icon} alt="work send icon" className=" w-3" />
             </div>
-          </div>
+          </a>
         </motion.div>
-        <motion.div
-          whileFocus={{ scale: 1.05 }}
-          transition={{ duration: 0.3 }}
-          className='aspect-square  bg-[url("/images/project-2.png")] bg-no-repeat bg-center  bg-cover rounded-lg relative cursor-pointer group'
-        >
-          <div className=" bg-white w-10/12 rounded-md absolute bottom-5 left-1/2 -translate-x-1/2 py-3 px-5 flex items-center justify-between duration-500 group-hover:bottom-7">
+        <motion.div whileFocus={{ scale: 1.05 }} transition={{ duration: 0.3 }} className='aspect-square  bg-[url("/images/project-2.png")] bg-no-repeat bg-center  bg-cover rounded-lg relative group'>
+          <a href="https://project2.kiamehr.dev/" className=" bg-white w-10/12 rounded-md absolute bottom-5 left-1/2 -translate-x-1/2 py-3 px-5 flex items-center justify-between duration-500 group-hover:bottom-7">
             <div>
               <h2 className=" font-semibold">{dict.work.projecttitle2}</h2>
               <p className=" text-xs text-gray-700">{dict.work.projectdescription2}</p>
             </div>
-            <a href="project1.kiamehr.dev" className=" border rounded-full border-black w-11 aspect-square flex items-center justify-center shadow:[2px_2px_0_#000] group-hover:bg-lime-300 transition">
+            <div className=" border rounded-full border-black w-11 aspect-square flex items-center justify-center shadow:[2px_2px_0_#000] group-hover:bg-lime-300 transition">
               <Image src={assets.send_icon} alt="work send icon" className=" w-3" />
-            </a>
-          </div>
+            </div>
+          </a>
         </motion.div>
-        <motion.div
-          whileFocus={{ scale: 1.05 }}
-          transition={{ duration: 0.3 }}
-          className='aspect-square  bg-[url("/images/project-3.png")] bg-no-repeat bg-center  bg-cover rounded-lg relative cursor-pointer group'
-        >
-          <div className=" bg-white w-10/12 rounded-md absolute bottom-5 left-1/2 -translate-x-1/2 py-3 px-5 flex items-center justify-between duration-500 group-hover:bottom-7">
+        <motion.div whileFocus={{ scale: 1.05 }} transition={{ duration: 0.3 }} className='aspect-square  bg-[url("/images/project-3.png")] bg-no-repeat bg-center  bg-cover rounded-lg relative group'>
+          <a href="https://project3.kiamehr.dev/" className=" bg-white w-10/12 rounded-md absolute bottom-5 left-1/2 -translate-x-1/2 py-3 px-5 flex items-center justify-between duration-500 group-hover:bottom-7">
             <div>
               <h2 className=" font-semibold">{dict.work.projecttitle3}</h2>
               <p className=" text-xs text-gray-700">{dict.work.projectdescription3}</p>
@@ -68,22 +59,7 @@ const Work = ({ dict }) => {
             <div className=" border rounded-full border-black w-11 aspect-square flex items-center justify-center shadow:[2px_2px_0_#000] group-hover:bg-lime-300 transition">
               <Image src={assets.send_icon} alt="work send icon" className=" w-3" />
             </div>
-          </div>
-        </motion.div>
-        <motion.div
-          whileFocus={{ scale: 1.05 }}
-          transition={{ duration: 0.3 }}
-          className='aspect-square  bg-[url("/images/project-4.png")] bg-no-repeat bg-center  bg-cover rounded-lg relative cursor-pointer group'
-        >
-          <div className=" bg-white w-10/12 rounded-md absolute bottom-5 left-1/2 -translate-x-1/2 py-3 px-5 flex items-center justify-between duration-500 group-hover:bottom-7">
-            <div>
-              <h2 className=" font-semibold">{dict.work.projecttitle4}</h2>
-              <p className=" text-xs text-gray-700">{dict.work.projectdescription4}</p>
-            </div>
-            <div className=" border rounded-full border-black w-11 aspect-square flex items-center justify-center shadow:[2px_2px_0_#000] group-hover:bg-lime-300 transition">
-              <Image src={assets.send_icon} alt="work send icon" className=" w-3" />
-            </div>
-          </div>
+          </a>
         </motion.div>
       </motion.div>
       <motion.a
